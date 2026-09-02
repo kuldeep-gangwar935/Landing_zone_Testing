6f33adb8b9c435d27145050dc54aab0759706b2f
Kuldeep Gangwar <krrishgangwar935@gmail.com>
2026-09-02 13:59:28 +0530

    Azure2

--- main.tf
+++ main.tf
@@ -1,22 +1,7 @@
 resource "azurerm_resource_group" "rg1" {
-    for_each = var.rgname
-    name=each.value
-    location=each.value
-  
-}
 
-
-resource "azurerm_resource_group" "rg2" {
-
-    name="rg-dev"
-    location="westus"
-  
-}
-
-resource "azurerm_resource_group" "rg3" {
-
-    name="rg-prod"
-    location="westus"
+    name=var.rgname
+    location=var.location
   
 }
 
--- variable.tf
+++ variable.tf
@@ -1,1 +1,2 @@
 variable "rgname" {}
+variable "location" {}
